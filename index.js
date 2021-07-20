ecbbcd84df35f428158c3dbff819dbe13c6eec60
mikiimikii <hajorge2012@gmail.com>
2021-07-20 13:34:16 -0300

    adding

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const display_before = document.getElementById('value_before');
 const display_actual = document.getElementById('value_actual');
-const botton_number = document.querySelectorAll('.number');
+const number_bottons = document.querySelectorAll('.number');
 const operator_bottons = document.querySelectorAll('.operator');
 
 class Calculator {
@@ -80,11 +80,10 @@ class Display {
 
 const display = new Display(display_before, display_actual);
 
-botton_number.forEach(boton => {
+number_bottons.forEach(boton => {
   boton.addEventListener('click', () => display.addNumber(boton.innerHTML));
 });
 
 operator_bottons.forEach(boton => {
   boton.addEventListener('click', () => display.computar(boton.value));
 });
-
